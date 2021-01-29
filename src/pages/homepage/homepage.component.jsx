dcafe942394973b7a5d59ef83bcdce19bb993403
Jacob Ujanga <jacobujanga@gmail.com>
2021-01-29 18:13:47 +0000

    updating our app with a shop page, collection preview component and collection item component

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import Directory from '../../components/directory.component.jsx/directory.component';
+import Directory from '../../components/directory.component/directory.component';
 
 import './homepage.styles.scss'
 
